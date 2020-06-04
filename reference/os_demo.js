a6b593d053bf6e8c80f937ef41b74d168b84a1f1
RichardVerbraak <RichardVerbraak@users.noreply.github.com>
2020-06-04 15:43:23 +0200

    Some cool funcs for info on your OS

---
+++ reference/os_demo.js
@@ -0,0 +1,22 @@
+const os = require('os')
+
+// Platform
+console.log(os.platform())
+
+// CPU Architecture
+console.log(os.arch())
+
+// CPU Core Info
+console.log(os.cpus())
+
+// Free memory available
+console.log(os.freemem())
+
+// Total memory
+console.log(os.totalmem())
+
+// Home Directory
+console.log(os.homedir())
+
+// System uptime
+console.log(os.uptime())
